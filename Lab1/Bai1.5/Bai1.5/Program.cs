b8b6c590b5712ba6d0216dd775464cd40d8832d8
Tran Kiet <kiettranuit@gmail.com>
2023-10-16 14:43:29 +0700

    Lab 1: bai 1.5

---
+++ Lab1/Bai1.5/Bai1.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Bai1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao duong dan den thu muc: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                string[] files = Directory.GetFiles(path);
+
+                Console.WriteLine("Danh sach tep tin trong thu muc " + path + ":");
+                for(int i = 0; i < files.Length; i++)
+                {
+                    Console.WriteLine(++i + ". " + files[i]);
+                }
+            } catch(DirectoryNotFoundException)
+            {
+                Console.WriteLine("Thu muc '" + path + "' khong ton tai!");
+            }
+        }
+    }
+}
