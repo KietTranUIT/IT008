52bd9d02764a8d7f89e183ab761ab9166bc4e346
hungbd31 <hungbd31@gmail.com>
2023-10-17 13:21:53 +0700

    Lab 1: bai 1.2

---
+++ Lab1/Bai1.2/Bai1.2/PhanSo.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1._2
+{
+        public class PhanSo : IComparable<PhanSo>
+        {
+            private int tuso;
+            private int mauso;
+
+            public PhanSo(int tuso = 0, int mauso = 1)
+            {
+                this.tuso = tuso;
+                this.mauso = mauso;
+            }
+
+            public int TuSo
+            {
+                get { return tuso; }
+                set { tuso = value; }
+            }
+
+            public int MauSo
+            {
+                get { return mauso; }
+                set
+                {
+                    if (value != 0)
+                        mauso = value;
+                    else
+                        throw new ArgumentException("Mau so phai khac 0!.");
+                }
+            }
+
+            public void RutGon()
+            {
+                int t = Math.Abs(tuso);
+                int m = Math.Abs(mauso);
+
+                while (m != 0)
+                {
+                    int temp = m;
+                    m = t % m;
+                    t = temp;
+                }
+                tuso /= t;
+                mauso /= t;
+            }
+
+            public int CompareTo(PhanSo o)
+            {
+                int a = tuso * o.mauso;
+                int b = mauso * o.tuso;
+                if (a > b) return 1;
+                else if (a < b) return -1;
+                else return 0;
+            }
+
+            public static bool operator >(PhanSo a, PhanSo b)
+            {
+                return a.CompareTo(b) > 0;
+            }
+
+            public static bool operator <(PhanSo a, PhanSo b)
+            {
+                return a.CompareTo(b) < 0;
+            }
+
+            public override bool Equals(object o)
+            {
+                if (o == null || GetType() != o.GetType()) return false;
+                PhanSo other = (PhanSo)o;
+                return tuso * other.mauso == mauso * other.tuso;
+            }
+
+            public override int GetHashCode()
+            {
+                return tuso.GetHashCode() ^ mauso.GetHashCode();
+            }
+
+            public static bool operator ==(PhanSo a, PhanSo b)
+            {
+                return a.CompareTo(b) == 0;
+            }
+
+            public static bool operator !=(PhanSo a, PhanSo b)
+            {
+                return !(a == b);
+            }
+
+            public static PhanSo operator +(PhanSo a, PhanSo b)
+            {
+                return new PhanSo((a.tuso * b.mauso) + (a.mauso * b.tuso), a.mauso * b.mauso);
+            }
+
+            public static PhanSo operator -(PhanSo a, PhanSo b)
+            {
+                return new PhanSo((a.tuso * b.mauso) - (a.mauso * b.tuso), a.mauso * b.mauso);
+            }
+
+            public static PhanSo operator *(PhanSo a, PhanSo b)
+            {
+                return new PhanSo(a.tuso * b.tuso, a.mauso * b.mauso);
+            }
+
+            public static PhanSo operator /(PhanSo a, PhanSo b)
+            {
+                return new PhanSo(a.tuso * b.mauso, a.mauso * b.tuso);
+            }
+
+            public static implicit operator PhanSo(int a)
+            {
+                return new PhanSo(a, 1);
+            }
+
+            public static explicit operator double(PhanSo a)
+            {
+                return (double)a.tuso / a.mauso;
+            }
+
+            public override string ToString()
+            {
+                RutGon();
+                return tuso + "/" + mauso;
+            }
+
+            public void Nhap()
+            {
+            Enhap:
+                try
+                {
+                    string ps = Console.ReadLine();
+                    string[] p = ps.Split('/');
+                    if (p.Length < 2 && int.TryParse(p[0], out tuso)) return;
+                    else if (p.Length == 2 && int.TryParse(p[0], out tuso) && int.TryParse(p[1], out mauso) && mauso != 0) return;
+                    else throw new ArgumentException("Chuoi khong hop le");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("Nhap lai");
+                    goto Enhap;
+                }
+            }
+
+        }
+    }
---
+++ Lab1/Bai1.2/Bai1.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1._2
+{
+        public static class Program
+        {
+            public static void Main()
+            {
+                bool exit = false;
+                while (!exit)
+                {
+                    Console.WriteLine("Menu :");
+                    Console.WriteLine("1. Tinh toan +, -, *, / 2 phan so. ");
+                    Console.WriteLine("2. Doi phan so sang so thuc.");
+                    Console.WriteLine("3. Sap xep 1 mang phan so.");
+                    Console.WriteLine("0. Thoat.");
+                    Console.WriteLine("Nhap lua chon cua ban");
+                    string select = Console.ReadLine();
+                    switch (select)
+                    {
+                        case "1":
+                            PhanSo a = new PhanSo();
+                            PhanSo b = new PhanSo();
+                            Console.WriteLine("Nhap phan so thu I:");
+                            a.Nhap();
+                            Console.WriteLine("Nhap phan so thu II:");
+                            b.Nhap();
+                            Console.WriteLine("Ket qua:");
+
+                            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + "{0}", a + b);
+                            Console.WriteLine(a.ToString() + " - " + b.ToString() + " = " + "{0}", a - b);
+                            Console.WriteLine(a.ToString() + " * " + b.ToString() + " = " + "{0}", a * b);
+                            Console.WriteLine(a.ToString() + " / " + b.ToString() + " = " + "{0}", a / b);
+                            break;
+
+                        case "2":
+                            PhanSo c = new PhanSo();
+                            Console.WriteLine("Nhap phan so can doi");
+                            c.Nhap();
+                            Console.WriteLine((double)c);
+                            break;
+
+                        case "3":
+                            int l;
+                            Console.WriteLine("Nhap so phan tu cua mang:");
+                            l = int.Parse(Console.ReadLine());
+                            PhanSo[] d = new PhanSo[l];
+                            int i = 0;
+                            while (i < l)
+                            {
+                                Console.WriteLine("Nhap phan tu thu {0}", i + 1);
+                                d[i] = new PhanSo();
+                                d[i].Nhap();
+                                i++;
+                            }
+                            Console.WriteLine("Sap xep mang.....");
+                            Console.WriteLine("Ket qua");
+                            Array.Sort(d);
+                            foreach (PhanSo x in d)
+                            {
+                                Console.WriteLine(x.ToString());
+                            }
+                            break;
+
+                        case "0":
+                            Console.WriteLine("Thoat chuong trinh");
+                        exit = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("Lua chon khong hop le, vui long chon lai.");
+                            break;
+                    }
+                }
+            }
+        }
+    }
