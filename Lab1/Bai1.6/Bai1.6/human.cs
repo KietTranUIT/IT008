1d11ef8f4f1a920585bacf9bb0fd949779683636
hungbd31 <hungbd31@gmail.com>
2023-10-17 13:48:23 +0700

    Lab1: bai 1.6

---
+++ Lab1/Bai1.6/Bai1.6/human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1._6
+{
+    class human : mamal, ithinking, iintelligent
+    {
+        public void thinking_behavior()
+        {
+
+        }
+        public void intelligent_behavior()
+        {
+
+        }
+
+        public human() { }
+    }
+}
---
+++ Lab1/Bai1.6/Bai1.6/ithingking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1._6
+{
+    
+    public interface ithinking
+    {
+        void thinking_behavior();
+    }
+}
